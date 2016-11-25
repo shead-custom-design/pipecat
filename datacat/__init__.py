9ac128214a080315e70c0b9e2112ab9461023820
Timothy M. Shead <tim@shead-custom-design.com>
2016-11-25 01:14:42 -0700

    First iteration of a generator-based design.

---
+++ datacat/__init__.py
@@ -0,0 +1,45 @@
+__version__ = "0.1.0-dev"
+
+import arrow
+
+import pint
+units = pint.UnitRegistry()
+
+def timestamp(source):
+    for observation in source:
+        observation["timestamp"] = arrow.utcnow()
+        yield observation
+
+def icharger(stream):
+    modes = {
+        1: "Charging",
+        2: "Discharging",
+        3: "Monitor",
+        4: "Waiting",
+        5: "Motor burn-in",
+        6: "Finished",
+        7: "Error",
+        8: "LIxx trickle",
+        9: "NIxx trickle",
+        10: "Foam cut",
+        11: "Info",
+        12: "External-discharging",
+    }
+
+    while True:
+        line = stream.readline()
+        raw = line.split(";")
+
+        observation = dict()
+        observation["mode"] = modes[int(raw[1])]
+        observation["supply-voltage"] = units.Quantity(float(raw[3]) / 1000, units.volts)
+        observation["battery-voltage"] = units.Quantity(float(raw[4]) / 1000, units.volts)
+        observation["charge-current"] = units.Quantity(float(raw[5]) * 10, units.milliamps)
+#    v_c1 = float (raw[6])/1000
+#    v_c2 = float (raw[7])/1000
+#    v_c3 = float (raw[8])/1000
+        observation["internal-temperature"] = units.Quantity(float(raw[14]) / 10, units.degC)
+        observation["external-temperature"] = units.Quantity(float(raw[15]) / 10, units.degC)
+        observation["charge-amount"] = units.Quantity(float(raw[16]) / 1000, units.amps * units.hours)
+
+        yield observation
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import re
+
+setup(
+    name="datacat",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Environment :: Other Environment",
+        "Framework :: IPython",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    description="Data logger for managing information collected from real-world instruments.",
+    install_requires=[
+        "arrow",
+        "numpy>=1.8.0",
+        "Pint",
+    ],
+    maintainer="Timothy M. Shead",
+    maintainer_email="tim@shead-custom-design.gov",
+    packages=find_packages(),
+    scripts=[
+        "bin/datacat-monitor",
+        ],
+    url="http://datacat.readthedocs.org",
+    version=re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]",
+        open(
+            "datacat/__init__.py",
+            "r").read(),
+        re.M).group(1),
+)
