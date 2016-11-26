cc72facafdadce05b6be59810ab715c0106eca64
Timothy M. Shead <tim@shead-custom-design.com>
2016-11-25 22:02:46 -0700

    Implement time-duration and timeout limits.

--- datacat/source/__init__.py
+++ datacat/source/__init__.py
@@ -8,22 +8,29 @@ import Queue
 
 import datacat
 
+
 def add(source, key, value):
     """Adds a key-value pair to every observation returned from another source."""
     for observation in source:
         datacat.store(observation, key, value)
         yield observation
 
+
 def limit(source, count):
     """Limits the number of observations returned from another source."""
-    for observation in itertools.islice(source, count):
+    for index, observation in enumerate(source):
+        if index >= count:
+            datacat.log.info("Stopped by %s observation limit." % count)
+            break
         yield observation
+
 
 def send_to_queue(source, queue):
     """Send observations from a source to a queue."""
     for observation in source:
         queue.put(observation)
     queue.put(StopIteration)
+
 
 def receive_from_queue(queue):
     """Receive observations from a queue."""
@@ -33,11 +40,13 @@ def receive_from_queue(queue):
             break
         yield observation
 
+
 def concatenate(sources):
     """Concatenate observations from multiple sources."""
     for source in sources:
         for observation in source:
             yield observation
+
 
 def multiplex(*sources):
     """Interleave observations from multiple sources."""
--- datacat/source/time.py
+++ datacat/source/time.py
@@ -2,7 +2,9 @@
 
 from __future__ import absolute_import, division, print_function
 
+import threading
 import time
+import Queue
 
 import arrow
 
@@ -14,9 +16,51 @@ def timestamp(source, key="timestamp"):
         datacat.store(observation, key, arrow.utcnow())
         yield observation
 
+
 def metronome(rate=datacat.quantity(1.0, datacat.units.seconds)):
     """Generate an empty observation at fixed time intervals."""
     while True:
         yield dict()
         time.sleep(rate.to(datacat.units.seconds).magnitude)
+
+
+def limit(source, duration, timeout=datacat.quantity(0.1, datacat.units.seconds)):
+    """Return observations from another source until a fixed time duration has expired."""
+    end_time = time.time() + duration.to(datacat.units.seconds).magnitude
+    queue_timeout = timeout.to(datacat.units.seconds).magnitude
+
+    queue = Queue.Queue()
+    thread = threading.Thread(target=datacat.source.send_to_queue, args=(source, queue))
+    thread.start()
+
+    while True:
+        if time.time() >= end_time:
+            datacat.log.info("Stopped by %s time limit." % duration)
+            break
+        try:
+            observation = queue.get(block=True, timeout=queue_timeout)
+        except Queue.Empty:
+            continue
+        if observation is StopIteration:
+            break
+        yield observation
+
+
+def timeout(source, timeout=datacat.quantity(5, datacat.units.seconds)):
+    """Return observations from another source until they stop arriving."""
+    queue_timeout = timeout.to(datacat.units.seconds).magnitude
+
+    queue = Queue.Queue()
+    thread = threading.Thread(target=datacat.source.send_to_queue, args=(source, queue))
+    thread.start()
+
+    while True:
+        try:
+            observation = queue.get(block=True, timeout=queue_timeout)
+        except Queue.Empty:
+            datacat.log.info("Stopped by %s timeout." % timeout)
+            break
+        if observation is StopIteration:
+            break
+        yield observation
 
