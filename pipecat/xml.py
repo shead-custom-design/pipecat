fd894a1f0549629c6862ddaa851103227c3f4b5b
Timothy M. Shead <tim@shead-custom-design.com>
2017-05-02 22:38:11 -0600

    Add functionality to parse XML data.

---
+++ pipecat/xml.py
@@ -0,0 +1,44 @@
+# Copyright 2017 Timothy M. Shead
+#
+# This file is part of Pipecat.
+#
+# Pipecat is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pipecat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pipecat.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Functions for working with XML data.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import xml.etree.ElementTree as xml
+
+import pipecat.record
+
+def parse(source, key="string"):
+    """Parse XML data from a record.
+
+    Parameters
+    ----------
+    source: :ref:`Record generator <record-generators>`, required
+    key: :ref:`Record key <record-keys>`, optional
+
+    Yields
+    ------
+    records: dict
+        Records will contain a single `xml` key containing XML parsed from a source record.
+    """
+    for record in source:
+        output = {}
+        pipecat.record.add_field(output, "xml", xml.fromstring(record[key]))
+        yield output
+
